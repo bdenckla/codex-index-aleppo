c165d6c77a6fd8594f159c8da83626f9a8a1c800
Ben Denckla <bdenckla@alum.mit.edu>
2026-01-29 15:24:24 -0500

    work on 4210 & 3719

--- pyauthor_qr/qr_4210_uxlc.py
+++ pyauthor_qr/qr_4210_uxlc.py
@@ -3,6 +3,20 @@ RECORD_4210_UXLC = {
     "qr-lc-proposed": "וַ֧יֹּסֶף",
     "qr-what-is-weird": "דרגא belongs to ו not $yod",
     "qr-consensus": "וַיֹּ֧סֶף",
+    "qr-comment": [
+        "In situations like this,",
+        " it is hard to determine which letter an under-accent belongs to."
+        " By “situations like this” I mean two narrow letters",
+        " (in this case ו and $yod)",
+        " where the first letter has an under-vowel and the second letter does not."
+        " In the face of this uncertainty,",
+        " our default should be to transcribe charitably,",
+        " i.e., all else equal, lean towards the consensus reading.",
+        " Aside: note that the פתח marks are problematic in both μL and μA,",
+        " for different reasons:",
+        " μL’s is faint and",
+        " μA’s is broken, looking a bit like a ציריה (!).",
+    ],
     "qr-highlight-lc-proposed": 1,
     "qr-highlight-consensus": 2,
     "qr-extra-letter-spacing": True,
@@ -10,4 +24,6 @@ RECORD_4210_UXLC = {
     "qr-bhq-comment": ["$BHQ is the source of the proposed transcription of μL above."],
     "qr-noted-by": "tBHQ-xBHL-xDM-xWLC-zUXLC",
     "qr-uxlc-change-url": "https://tanach.us/Changes/2023.10.19%20-%20Changes/2023.10.19%20-%20Changes.xml?2023.06.10-48",
+    "qr-aleppo-img": "Aleppo-4210.png",
+    "qr-aleppo-page-url": "https://www.mgketer.org/mikra/29/42/1/mg/106",
 }
