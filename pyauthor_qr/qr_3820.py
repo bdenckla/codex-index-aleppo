1bf274d092b31262384b3a3fd959852532242937
Ben Denckla <bdenckla@alum.mit.edu>
2026-01-31 16:28:36 -0500

    38:20

--- pyauthor_qr/qr_3820.py
+++ pyauthor_qr/qr_3820.py
@@ -1,6 +1,3 @@
-from pyauthor_util.uxlc_change import uxlc_change
-
-
 RECORD_3820 = {
     "qr-cv": "38:20",
     "qr-lc-proposed": "תָ֝בִין",
@@ -9,11 +6,7 @@ RECORD_3820 = {
     "qr-generic-comment": [
         "The only mark above the ב is a רפה.",
     ],
-    "qr-lc-img": "3820-custom.png",
-    # Above, we use our own screen snip (a .png) rather than one taken
-    # from the UXLC change record (a .jpg), which is what we do in all other cases.
     "qr-highlight": 2,
     "qr-lc-loc": {"page": "408A", "column": 1, "line": 24},
-    "qr-noted-by": "tBHQ-xBHL-xDM-xWLC-nUXLC",
-    "qr-uxlc-change-url": uxlc_change("2023.10.19", "2023.06.10-45"),
+    "qr-noted-by": "tBHQ-xBHL-nDM",
 }
