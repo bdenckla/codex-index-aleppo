7dae37b51b8accf870eef0954c325c0987f4fcfc
Ben Denckla <bdenckla@alum.mit.edu>
2026-01-31 21:56:03 -0500

    0610, 0617, & 0621

--- pyauthor_qr/qr_0610.py
+++ pyauthor_qr/qr_0610.py
@@ -2,6 +2,7 @@ RECORD_0610 = {
     "qr-noted-by": "xBHQ-nBHL-nDM",
     "qr-cv": "6:10",
     "qr-consensus": "וּ֥תְהִי־ע֨וֹד ׀",
+    "qr-lc-proposed": "וּ֥תְהִי ע֨וֹד ׀",
     "qr-what-is-weird": "No maqaf",
     "qr-lc-loc": {"page": "398B", "column": 2, "line": 7},
     "qr-under-construction": True,
--- pyauthor_qr/qr_0621.py
+++ pyauthor_qr/qr_0621.py
@@ -1,8 +1,9 @@
 RECORD_0621 = {
-    "qr-noted-by": "xBHQ-nBHL-nDM",
+    "qr-noted-by": "nBHQ-xBHL-nDM",
     "qr-cv": "6:21",
     "qr-consensus": "לֹ֑א",
-    "qr-what-is-weird": "k/q difference; noted by BHQ (DM footnotes 10 & 11)",
+    "qr-lc-proposed": "ל֑וֹ",
+    "qr-what-is-weird": "קרי of לו not לא (DM footnotes 10 & 11)",
     "qr-lc-loc": {"page": "398B", "column": 2, "line": 20},
     "qr-under-construction": True,
 }
