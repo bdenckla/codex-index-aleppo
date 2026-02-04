8f135023ba790197a32fa50f457e312bd8b80029
Ben Denckla <bdenckla@alum.mit.edu>
2026-02-04 16:06:32 -0500

    3212

--- pyauthor_qr/qr_3212.py
+++ pyauthor_qr/qr_3212.py
@@ -1,8 +1,9 @@
 RECORD_3212 = {
     "qr-noted-by": "nDM",
     "qr-cv": "32:12",
+    "qr-lc-proposed": "וְעָֽדֵיכֶ֗ם",
     "qr-consensus": "וְעָ֥דֵיכֶ֗ם",
-    "qr-what-is-weird": "געיה not מרכא. How would you tell?",
+    "qr-what-is-weird": "געיה not מרכא",
     "qr-lc-loc": {"page": "406A", "column": 2, "line": 3},
     "qr-under-construction": True,
 }
